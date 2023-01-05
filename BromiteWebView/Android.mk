48a9d78345e40db548b58ebef28873a85224fc7c
walternewtz <77871609+walternewtz@users.noreply.github.com>
2023-01-05 22:31:49 +0530

    Update Android.mk

--- BromiteWebView/Android.mk
+++ BromiteWebView/Android.mk
@@ -1,5 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
-LOCAL_PACKAGE_NAME := treble-overlay-bromite-webview
+LOCAL_PACKAGE_NAME := power-overlay
 LOCAL_IS_RUNTIME_RESOURCE_OVERLAY := true
 include $(BUILD_PACKAGE)
